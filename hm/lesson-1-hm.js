dcb33f3ff05ba4d009a5bca79dd744bf9f6c81eb
danillevko <danillevko1212@gmail.com>
2026-01-10 13:19:55 -0700

    done

--- hm/lesson-1-hm.js
+++ hm/lesson-1-hm.js
@@ -3,9 +3,20 @@ const img = document.querySelector('.slider__image');
 
 let lastValue = range.value;
 
-range.addEventListener('change', () => {
+range.addEventListener('change', _.debounce(() => {
   if (range.value !== lastValue) {
     img.style.width = range.value * 3 + 'px';
     lastValue = range.value;
   }
-});
+}, 100));
+
+
+const box = document.querySelector('#box');
+const svg = document.querySelector('svg')
+svg.addEventListener('mousemove', _.debounce((event)=>{
+    let rect = svg.getBoundingClientRect();
+    let coordsX = event.clientX - rect.left;
+    let coordsY = event.clientY - rect.top;
+    box.setAttribute('cx', coordsX);
+    box.setAttribute('cy', coordsY);
+}, 15));
--- lesson 2/lesson-2.js
+++ lesson 2/lesson-2.js
@@ -1,3 +1,4 @@
+const input = document.querySelector('#input');
 // const result = _.add(1, 3);
 // console.log(result);
 
@@ -11,8 +12,29 @@
 // }, 1000))
 
 
-window.addEventListener('mousemove', _.debounce(alo, 1000));
+// window.addEventListener('mousemove', _.debounce(alo, 1000));
 
-function alo(){
-    console.log('alo');
-};
+// function alo(){
+//     console.log('alo');
+// };
+
+// input.addEventListener('input', _.throttle(ip, 3000));
+
+// function ip(){
+//     console.log('ip');
+// }
+
+// input.addEventListener('input', _.debounce(()=>{
+//     alert(`Hello ${input.value}`);
+// }, 2500));
+
+
+const graph = document.querySelector('#graph');
+const circle = document.querySelector('#point');
+graph.addEventListener('mousemove', _.throttle((event)=>{
+    let rect = graph.getBoundingClientRect();
+    let coordsX = event.clientX - rect.left;
+    let coordsY = event.clientY - rect.top;
+    circle.setAttribute('cx', coordsX);
+    circle.setAttribute('cy', coordsY);
+}, 0))
